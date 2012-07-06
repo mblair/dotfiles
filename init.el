0bc224f4ab9705802cff44ede1f20e7c6409ec6a
Matt Blair <me@matthewblair.net>
2012-07-06 16:11:48 -0700

    add emacs file that i use with ESK

---
+++ init.el
@@ -0,0 +1,13 @@
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(defvar mah-packages '(starter-kit starter-kit-lisp starter-kit-ruby)
+  "A list of packages that I want to ensure are installed at launch.")
+
+(add-to-list 'custom-theme-load-path "~/git_src/emacs-color-theme-solarized/")
+(load-theme 'solarized-dark t)
+
+(setq-default visible-bell nil)
+
+(global-linum-mode 1)
