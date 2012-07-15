b45438e72e3816861743b2b14294a5cfac0da90e
Matt Blair <me@matthewblair.net>
2012-07-15 11:31:06 -0700

    the emacs solarized folks broke something.

--- init.el
+++ init.el
@@ -5,7 +5,8 @@
 (defvar mah-packages '(starter-kit starter-kit-lisp starter-kit-ruby)
   "A list of packages that I want to ensure are installed at launch.")
 
-(add-to-list 'custom-theme-load-path "~/git_src/emacs-color-theme-solarized/")
+;;(add-to-list 'custom-theme-load-path "~/git_src/emacs-color-theme-solarized/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/color-theme-solarized-20120301")
 (load-theme 'solarized-dark t)
 
 (setq-default visible-bell nil)
