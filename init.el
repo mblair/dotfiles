7cd2a00179b8ff05ee51a901153ab8ebc0f6c819
Matt Blair <me@matthewblair.net>
2013-05-09 19:19:14 -0700

    maybe i'll review my diffs from now on eh

--- init.el
+++ init.el
@@ -102,15 +102,3 @@
 ;; (load-theme 'telstar t)
 ;; (add-to-list 'custom-theme-load-path "~/external_src/color-theme-sanityinc-solarized")
 ;; (load-theme 'sanityinc-solarized-dark t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("787574e2eb71953390ed2fb65c3831849a195fd32dfdd94b8b623c04c7f753f0" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
