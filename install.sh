f69e46192ba910b8838eb0bf068d78a6655586f4
Matt Blair <me@matthewblair.net>
2013-05-14 16:43:41 -0700

    fix installer script

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ set -o xtrace
 set -o nounset
 
 mkdir ~/.emacs.d
-ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
+ln -s ~/my_src/dotfiles/init.el ~/.emacs.d/init.el
 /usr/bin/emacs --daemon
 
-ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore_global ~/.gitignore
-ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/my_src/dotfiles/bashrc ~/.bashrc
+ln -s ~/my_src/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/my_src/dotfiles/gitignore_global ~/.gitignore
+ln -s ~/my_src/dotfiles/inputrc ~/.inputrc
+ln -s ~/my_src/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/my_src/dotfiles/osx ~/.osx
+ln -s ~/my_src/dotfiles/rtorrentrc ~/.rtorrent.rc
+ln -s ~/my_src/dotfiles/hgrc ~/.hgrc
