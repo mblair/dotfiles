f8380beca25726e703ed3086709d09fca239bb73
Matt Blair <me@matthewblair.net>
2013-05-28 11:46:11 -0700

    mac/linux installation quirks

--- install.sh
+++ install.sh
@@ -4,15 +4,33 @@ set -o errexit
 set -o xtrace
 set -o nounset
 
-mkdir ~/.emacs.d
-ln -s ~/my_src/dotfiles/init.el ~/.emacs.d/init.el
-/usr/bin/emacs --daemon
+mkdir -p ~/.emacs.d
+ln -sf ~/my_src/dotfiles/init.el ~/.emacs.d/init.el
 
-ln -sf ~/my_src/dotfiles/bashrc ~/.bashrc
+if [[ $(uname -s) == "Darwin" ]]; then
+  _EMACS=/usr/local/bin/emacs
+else
+  _EMACS=/usr/bin/emacs
+fi
+
+if [[ $(uname -s) == "Darwin" ]]; then
+  _BASH_RC=~/.bash_profile
+else
+  _BASH_RC=~/.bashrc
+fi
+
+ln -sf ~/my_src/dotfiles/bashrc ${_BASH_RC}
 ln -s ~/my_src/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/my_src/dotfiles/gitignore_global ~/.gitignore
 ln -s ~/my_src/dotfiles/inputrc ~/.inputrc
 ln -s ~/my_src/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/my_src/dotfiles/osx ~/.osx
-ln -s ~/my_src/dotfiles/rtorrentrc ~/.rtorrent.rc
+
+mkdir .irssi
+ln -s ~/my_src/dotfiles/irssi_config ~/.irssi/config
+
 ln -s ~/my_src/dotfiles/hgrc ~/.hgrc
+
+if [[ $(uname -s) == "Darwin" ]]; then
+  ln -s ~/my_src/dotfiles/osx ~/.osx
+  ln -s ~/my_src/dotfiles/rtorrentrc ~/.rtorrent.rc
+fi
