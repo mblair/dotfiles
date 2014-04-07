7f0ffc8d876cc2f02e3e810749e900f701816b4a
Matt Blair <me@matthewblair.net>
2014-04-07 01:54:54 -0700

    lein: add eastwood

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [
                   [lein-ring "0.8.10"]
                   [lein-ancient "0.5.4"]
+                  [jonase/eastwood "0.1.2"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.2.0"]]
