5de61e78201abfa6b457ca096c4f8b2376f7d8fa
Matt Blair <me@matthewblair.net>
2014-05-03 08:41:35 -0700

    lein: dep updates, add cider-nrepl

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,8 +1,9 @@
 {:user {:plugins [
                   [lein-ring "0.8.10"]
-                  [lein-ancient "0.5.4"]
+                  [lein-ancient "0.5.5"]
                   [jonase/eastwood "0.1.2"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
                   ]}}
 {:dev {:dependencies
-       [[com.cemerick/pomegranate "0.2.0"]]
+       [[com.cemerick/pomegranate "0.3.0"]]
        }}
