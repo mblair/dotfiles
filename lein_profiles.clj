eac2733135fb587f3b81f3f3604df855182c36e5
Matt Blair <me@matthewblair.net>
2014-08-06 10:23:16 -0700

    cider-nrepl 0.7.0 is final

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -2,7 +2,7 @@
                   [lein-ring "0.8.11"]
                   [lein-ancient "0.5.5"]
                   [jonase/eastwood "0.1.4"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.7.0"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.3.0"]]
