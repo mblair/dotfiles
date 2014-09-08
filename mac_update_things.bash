a3d6fdb9fa52e216f266e7a0efc35db6a62203c5
Matt Blair <me@matthewblair.net>
2014-09-08 09:28:48 -0700

    mac updater: update errcheck

--- mac_update_things.bash
+++ mac_update_things.bash
@@ -59,6 +59,7 @@ export GOPATH="$HOME/gopath"
 go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u github.com/golang/lint/golint
+go get -u github.com/kisielk/errcheck
 go get -u code.google.com/p/go.tools/cmd/{cover,godoc,goimports,oracle,vet}
 
 # cd ~/rebuild_src/etcd
