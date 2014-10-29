12f00c463a702f2cd99c97be2998319c28179bb3
Matt Blair <me@matthewblair.net>
2014-10-29 10:07:53 -0700

    stuff.

--- mac_update_things.bash
+++ mac_update_things.bash
@@ -86,8 +86,8 @@ brew reinstall --HEAD hub rbenv ruby-build etcdctl
 #     tic -o ~/.terminfo etc/e/eterm-color.ti
 # )
 
-#rm -rf ~/.emacs.d; mkdir -p ~/.emacs.d/; ln -s ~/my_src/dotfiles/init.el ~/.emacs.d;
-rm -r ~/.emacs.d; cd ~/external_src/prelude; git clean -fdx; git pull; ln -s ~/external_src/prelude ~/.emacs.d; cp ~/external_src/prelude/sample/prelude-modules.el ~/.emacs.d/; echo "(require 'prelude-go)" >> ~/.emacs.d/prelude-modules.el; echo "(require 'prelude-clojure)" >> ~/.emacs.d/prelude-modules.el; ln -s ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal; /usr/local/bin/emacs --daemon
+rm -rf ~/.emacs.d; mkdir -p ~/.emacs.d/; ln -s ~/my_src/dotfiles/init.el ~/.emacs.d;
+#rm -r ~/.emacs.d; cd ~/external_src/prelude; git clean -fdx; git pull; ln -s ~/external_src/prelude ~/.emacs.d; cp ~/external_src/prelude/sample/prelude-modules.el ~/.emacs.d/; echo "(require 'prelude-go)" >> ~/.emacs.d/prelude-modules.el; echo "(require 'prelude-clojure)" >> ~/.emacs.d/prelude-modules.el; ln -s ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal; /usr/local/bin/emacs --daemon
 
 npm install -g npm@2.0.2
 npm update -g groc bower yo grunt-cli generator-angular chartjs
--- prelude/personal.el
+++ prelude/personal.el
@@ -3,3 +3,10 @@
 (setq prelude-whitespace nil)
 
 (setq prelude-theme 'deeper-blue)
+
+(global-linum-mode 1)
+(global-set-key (kbd "M-l") 'goto-line)
+
+(if (not (string-match "go" compile-command))
+    (set (make-local-variable 'compile-command)
+         "make"))
