ce709e14aba9237f86b96c91e2981af71a0a9772
Matt Blair <me@matthewblair.net>
2014-11-18 06:39:31 -0800

    lein: eastwood 0.2.0

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [
                   [lein-ring "0.8.13"]
                   [lein-ancient "0.5.5"]
-                  [jonase/eastwood "0.1.5"]
+                  [jonase/eastwood "0.2.0"]
                   [cider/cider-nrepl "0.7.0"]
                   ]}}
 {:dev {:dependencies
