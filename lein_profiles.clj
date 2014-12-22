16b3c4c75e9b8541221e2d9496ba86ef350b7e0a
Matt Blair <me@matthewblair.net>
2014-12-21 23:18:14 -0500

    lein: cider-nrepl 0.8.2

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -2,7 +2,7 @@
                   [lein-ring "0.8.13"]
                   [lein-ancient "0.5.5"]
                   [jonase/eastwood "0.2.0"]
-                  [cider/cider-nrepl "0.8.1"]
+                  [cider/cider-nrepl "0.8.2"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.3.0"]]
