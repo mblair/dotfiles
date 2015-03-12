4855809e89369ff986f45aefb0d313855cac9541
Matt Blair <me@matthewblair.net>
2015-03-12 00:06:47 -0700

    mac updater: try prelude again

--- mac_update_things.bash
+++ mac_update_things.bash
@@ -19,7 +19,6 @@ git pull
 cd ~/external_src/go-mode.el
 git pull
 
-# export GOROOT="$HOME/goroot"
 export GOPATH="$HOME/gopath"
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
@@ -33,28 +32,18 @@ brew reinstall --HEAD hub rbenv ruby-build
 
 go get -u github.com/coreos/etcd/etcdctl
 
-if [[ -d ~/external_src/prelude ]]; then
-    cd ~/external_src/prelude
-    git clean -fdx
-    git pull
-else
-    cd ~/external_src
-    git clone https://github.com/bbatsov/prelude
-fi
-
-
 rm -rf ~/.emacs.d
-mkdir -p ~/.emacs.d/; ln -s ~/my_src/dotfiles/init.el ~/.emacs.d
+# mkdir -p ~/.emacs.d/; ln -s ~/my_src/dotfiles/init.el ~/.emacs.d
 
-# ln -s ~/external_src/prelude ~/.emacs.d
-# cp ~/external_src/prelude/sample/prelude-modules.el ~/.emacs.d/
-# cat >> ~/.emacs.d/prelude-modules.el <<EOF
-# (require 'prelude-helm)
-# (require 'prelude-helm-everywhere)
-# (require 'prelude-go)
-# (require 'prelude-clojure)
-# EOF
-# ln -s ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal
+ln -s ~/external_src/prelude ~/.emacs.d
+cp ~/external_src/prelude/sample/prelude-modules.el ~/.emacs.d/
+cat >> ~/.emacs.d/prelude-modules.el <<EOF
+(require 'prelude-helm)
+(require 'prelude-helm-everywhere)
+(require 'prelude-go)
+(require 'prelude-clojure)
+EOF
+ln -s ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal
 
 /usr/local/bin/emacs --daemon
 
