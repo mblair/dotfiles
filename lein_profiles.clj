d259ac9ed809927b3255d4e6c7c9a7a4735d29a1
Matt Blair <me@matthewblair.net>
2015-04-04 19:12:59 -0700

    lein: cider-nrepl bump

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -2,7 +2,7 @@
                   [lein-ring "0.9.3"]
                   [lein-ancient "0.6.5"]
                   [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.8.2"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.3.0"]]
