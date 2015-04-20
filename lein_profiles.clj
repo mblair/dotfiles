45dc106dddce64ebbcf08b169108c4d271c770be
Matt Blair <me@matthewblair.net>
2015-04-19 23:17:18 -0700

    lein: lein-ancient bump

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [
                   [lein-ring "0.9.3"]
-                  [lein-ancient "0.6.6"]
+                  [lein-ancient "0.6.7"]
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   ]}}
