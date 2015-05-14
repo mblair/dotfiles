c4b37e3b07d5a85ad1271d35862b45411075682c
Matt Blair <me@matthewblair.net>
2015-05-13 21:12:35 -0700

    emacs: switch theme

--- init.el
+++ init.el
@@ -133,7 +133,7 @@
 ;; http://stackoverflow.com/questions/7616761/even-when-emacsclient-is-started-in-a-terminal-window-system-is-non-nil
 (defun color-config (&optional frame)
   (select-frame frame)
-  (if window-system (load-theme 'cherry-blossom t)
+  (if window-system (load-theme 'base16-ocean-dark t)
     (load-theme 'zenburn t)))
 
 ;; for emacsclient:
