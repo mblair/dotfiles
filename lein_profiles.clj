2eea76f4169d6a7d74c7a63cf127304fb5b74eb5
Matt Blair <me@matthewblair.net>
2015-05-13 21:12:44 -0700

    lein: new lein-ring

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [
-                  [lein-ring "0.9.3"]
+                  [lein-ring "0.9.4"]
                   [lein-ancient "0.6.7"]
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
