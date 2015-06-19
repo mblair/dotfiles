d09757d8a9100943067fd2d3d1d94a99eca3f0f0
Matt Blair <me@matthewblair.net>
2015-06-18 18:49:41 -0700

    lein: bumps

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [
-                  [lein-ring "0.9.4"]
+                  [lein-ring "0.9.6"]
                   [lein-ancient "0.6.7"]
                   [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.9.0"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.3.0"]]
