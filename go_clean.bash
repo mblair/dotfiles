229b7cdc431ee569a898b4a71454e8c451836126
Matt Blair <me@matthewblair.net>
2015-07-19 10:38:26 -0700

    go cleaner: re-clone deleted clones

--- go_clean.bash
+++ go_clean.bash
@@ -7,7 +7,7 @@ _current_employer_github_org=Flipboard
 _current_employer_gopath="${GOPATH}/src/github.com/${_current_employer_github_org}"
 _unpushed_changes=0
 _dirty_repos=()
-
+_to_clone=()
 if [[ -d "${_current_employer_gopath}" ]]; then
     cd "${_current_employer_gopath}"
     for _inner in ./*; do
@@ -17,13 +17,16 @@ if [[ -d "${_current_employer_gopath}" ]]; then
                 mv "${_current_employer_gopath}/${_inner}" /tmp
                 _dirty_repos+=${_inner}
                 _unpushed_changes=1
+            else
+                _to_clone+=$(git remote -v show origin | grep Fetch | cut -d":" -f2- | tr -d '[:space:]')
             fi
         fi
     done
 fi
 
 rm -rf "$GOPATH"
-mkdir -p "$GOPATH"
+mkdir -p "${_current_employer_gopath}"
+cd
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
 go get -u github.com/golang/lint/golint
@@ -40,5 +43,12 @@ if [[ ${_unpushed_changes} == 1 ]]; then
     cd "${_current_employer_gopath}"
     for _repo in ${_dirty_repos[*]}; do
         mv /tmp/"${_repo}" .
+    done
+fi
+
+if [[ ${#_to_clone[*]} -gt 0 ]]; then
+    for _url in ${_to_clone[*]}; do
+        cd "${_current_employer_gopath}"
+        git clone "${_url}"
     done
 fi
