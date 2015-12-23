510e307dc5d66d1b1754f97e6903740d3fc2e90f
Matt Blair <me@matthewblair.net>
2015-12-23 16:36:37 -0500

    lein: update all the things

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [
-                  [lein-ring "0.9.6"]
-                  [lein-ancient "0.6.7"]
-                  [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.9.1"]
+                  [lein-ring "0.9.7"]
+                  [lein-ancient "0.6.8"]
+                  [jonase/eastwood "0.2.3"]
+                  [cider/cider-nrepl "0.10.0"]
                   ]}}
 {:dev {:dependencies
        [[com.cemerick/pomegranate "0.3.0"]]
