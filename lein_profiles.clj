d5cddab89bbc48e90a2857df24136bca31b2f6c7
Matt Blair <me@matthewblair.net>
2016-01-19 19:07:50 -0800

    lein: remove unused plugins

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,11 +1,5 @@
 {:user {:plugins [
                   [lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
-                  [lein-ring "0.9.7"]
                   [lein-ancient "0.6.8"]
-                  [jonase/eastwood "0.2.3"]
-                  [cider/cider-nrepl "0.10.1"]
                   ]}}
-{:dev {:dependencies
-       [[com.cemerick/pomegranate "0.3.0"]]
-       }}
