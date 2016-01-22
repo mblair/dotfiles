dfb3ec12d2574aa3f5013e7662c7439db9c41d26
Matt Blair <me@matthewblair.net>
2016-01-22 13:56:42 -0800

    need cider-nrepl

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [
                   [lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.8"]
+                  [cider/cider-nrepl "0.10.1"]
                   ]}}
