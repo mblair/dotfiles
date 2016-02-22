3b30b7a12d58c20f91b92d325c3221ea64e02a39
Matt Blair <me@matthewblair.net>
2016-02-22 09:26:09 -0800

    tryin out howdoi

--- mac_update_things.bash
+++ mac_update_things.bash
@@ -13,7 +13,7 @@ gem cleanup --quiet
 # check to make sure we're using the virtualenv python here
 easy_install -U setuptools
 #pip install -U pip
-pip install -U s3cmd autopep8 virtualenv
+pip install -U s3cmd autopep8 virtualenv howdoi
 pip freeze | cut -d= -f1 | env grep -v git-remote-helpers | env grep -v wsgiref | xargs pip install -U
 
 cd ~/.oh-my-zsh
