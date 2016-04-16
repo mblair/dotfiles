434375f72baf5b9e10db2a65cb40ddaff2a8381c
Matt Blair <me@matthewblair.net>
2016-04-16 14:03:36 -0700

    lein: updates

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [
-                  [lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
+                  [lein-cljfmt "0.5.1" :exclusions [org.clojure/clojure]]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
                   [lein-cloverage "1.0.6"]
                   [org.clojure/tools.nrepl "0.2.12"]
                   [cider/cider-nrepl "0.11.0"]
