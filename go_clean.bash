95502f46954f377be226af3a29e6f6b280ca60c4
Matt Blair <me@matthewblair.net>
2016-09-16 17:25:53 -0700

    i'm no longer a flipboard gopher

--- go_clean.bash
+++ go_clean.bash
@@ -3,31 +3,8 @@
 set -xueo pipefail
 
 export GOPATH="$HOME/gopath"
-_current_employer_github_org=Flipboard
-_current_employer_gopath="${GOPATH}/src/github.com/${_current_employer_github_org}"
-_unpushed_changes=0
-_dirty_repos=()
-_to_clone=()
-
-if [[ -d "${_current_employer_gopath}" ]]; then
-    cd "${_current_employer_gopath}"
-    for _inner in ${_current_employer_gopath}/*; do
-        echo "${_inner}"
-        if [[ -d "${_inner}" ]]; then
-            cd "${_inner}"
-            if [[ $(git log --branches --not --remotes) != "" ]] || ! git diff --quiet HEAD || test -n "$(git ls-files --others)"; then
-                mv "${_inner}" /tmp
-                _dirty_repos+=$(basename ${_inner})
-                _unpushed_changes=1
-            else
-                _to_clone+=($(git remote -v show origin | grep Fetch | cut -d":" -f2- | tr -d '[:space:]'))
-            fi
-        fi
-    done
-fi
-
 rm -rf "$GOPATH"
-mkdir -p "${_current_employer_gopath}"
+
 cd
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
@@ -45,18 +22,3 @@ go get -u github.com/tpng/gopkgs
 go get -u github.com/sqs/goreturns
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install
-
-if [[ ${_unpushed_changes} == 1 ]]; then
-    mkdir -p "${_current_employer_gopath}"
-    cd "${_current_employer_gopath}"
-    for _repo in ${_dirty_repos[*]}; do
-        mv /tmp/"${_repo}" .
-    done
-fi
-
-if [[ ${#_to_clone[*]} -gt 0 ]]; then
-    for _url in ${_to_clone[*]}; do
-        cd "${_current_employer_gopath}"
-        git clone "${_url}"
-    done
-fi
