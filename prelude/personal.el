15ce214916d337fb6dd8e65f59f55a9422e764a1
Matt Blair <me@matthewblair.net>
2016-10-19 16:47:25 -0700

    emacs: more prelude customizations

--- prelude/personal.el
+++ prelude/personal.el
@@ -2,10 +2,11 @@
 (setq mac-option-modifier 'super)
 (setq prelude-whitespace nil)
 
-(setq prelude-theme 'deeper-blue)
-
 (global-linum-mode 1)
 (global-set-key (kbd "M-l") 'goto-line)
 (global-set-key "\M-c" 'clipboard-kill-ring-save)
 (global-set-key "\M-v" 'clipboard-yank)
 (global-set-key "\C-c c" 'comment-dwim)
+(set-face-attribute 'default nil :height 140)
+(prelude-require-package 'planet-theme)
+(load-theme 'planet t)
