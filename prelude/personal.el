8ea3476fe6b47740d51d02c8583eacb02f64c9cd
Matt Blair <me@matthewblair.net>
2016-11-06 09:27:16 -0800

    emacs remove useless comments

--- prelude/personal.el
+++ prelude/personal.el
@@ -1,16 +1,12 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 (setq prelude-whitespace nil)
-
 (global-linum-mode 1)
 (global-set-key (kbd "M-l") 'goto-line)
-;;(global-set-key "\M-c" 'clipboard-kill-ring-save)
-;;(global-set-key "\M-v" 'clipboard-yank)
 (global-set-key "\C-c c" 'comment-dwim)
 (prelude-require-package 'planet-theme)
 (prelude-require-package 'ag)
 (load-theme 'planet t)
 (set-face-attribute 'default nil :height 140)
-;; http://sourcefoundry.org/hack/
 (set-face-attribute 'default nil :family "Droid Sans Mono")
 (setq whitespace-style (remove'lines-tail whitespace-style))
