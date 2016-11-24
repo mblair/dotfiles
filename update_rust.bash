96d8a7676e528aae56258306f801fe0b7f1f3581
Matt Blair <me@matthewblair.net>
2016-11-23 18:42:12 -0800

    stop thrashing racer and rustfmt, and add fast-cli

--- update_rust.bash
+++ update_rust.bash
@@ -5,10 +5,10 @@ set -xueo pipefail
 multirust update nightly
 multirust default nightly
 
-for _pkg in racer rustfmt; do
-    if which ${_pkg}; then
-        cargo uninstall "${_pkg}"
-    fi
-
-    cargo install "${_pkg}"
-done
+# for _pkg in racer rustfmt; do
+#     if which ${_pkg}; then
+#         cargo uninstall "${_pkg}"
+#     fi
+#
+#     cargo install "${_pkg}"
+# done
