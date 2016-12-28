1bfed7ad9adef10f2bac35be316a9b6d5560ec83
Matt Blair <me@matthewblair.net>
2016-12-28 13:50:58 -0500

    go cleaner: don't rebuild GOPATH

--- go_clean.sh
+++ go_clean.sh
@@ -6,49 +6,52 @@ if [[ -z ${GOPATH:-} ]]; then
 	export GOPATH="$HOME/go"
 fi
 
-_current_employer_github_org=figma
-_current_employer_gopath="${GOPATH}/src/github.com/${_current_employer_github_org}"
-_unpushed_changes=0
-_dirty_repos=()
-_to_clone=()
-
-if [[ -d "${_current_employer_gopath}" ]]; then
-	cd "${_current_employer_gopath}"
-	for _inner in ${_current_employer_gopath}/*; do
-		echo "${_inner}"
-		if [[ -d "${_inner}" ]]; then
-			cd "${_inner}"
-			if [[ $(git log --branches --not --remotes) != "" ]] || ! git diff --quiet HEAD || test -n "$(git ls-files --others)"; then
-				mv "${_inner}" /tmp
-				_dirty_repos+=$(basename ${_inner})
-				_unpushed_changes=1
-			else
-				_to_clone+=($(git remote -v show origin | grep Fetch | cut -d":" -f2- | tr -d '[:space:]'))
-			fi
-		fi
-	done
-fi
-
-rm -rf "$GOPATH"
-mkdir -p "${_current_employer_gopath}"
-
-if [[ ${_unpushed_changes} == 1 ]]; then
-	mkdir -p "${_current_employer_gopath}"
-	cd "${_current_employer_gopath}"
-	for _repo in ${_dirty_repos[*]}; do
-		cd "${_current_employer_gopath}"
-		mv /tmp/"${_repo}" .
-		cd ${_repo}
-		git remote prune origin
-	done
-fi
-
-if [[ ${#_to_clone[*]} -gt 0 ]]; then
-	for _url in ${_to_clone[*]}; do
-		cd "${_current_employer_gopath}"
-		git clone "${_url}"
-	done
-fi
+# _current_employer_github_org=figma
+# _github_gopath_prefix="${GOPATH}/src/github.com"
+# _current_employer_gopath="${_github_gopath_prefix}/${_current_employer_github_org}"
+# _unpushed_changes=0
+#
+# # TODO: use two arrays here, one for personal, one for work
+# _dirty_repos=()
+# _to_clone=()
+#
+# if [[ -d "${_current_employer_gopath}" ]]; then
+# 	cd "${_current_employer_gopath}"
+# 	for _inner in ${_current_employer_gopath}/*; do
+# 		echo "${_inner}"
+# 		if [[ -d "${_inner}" ]]; then
+# 			cd "${_inner}"
+# 			if [[ $(git log --branches --not --remotes) != "" ]] || ! git diff --quiet HEAD || test -n "$(git ls-files --others)"; then
+# 				mv "${_inner}" /tmp
+# 				_dirty_repos+=("$(basename "${_inner}")")
+# 				_unpushed_changes=1
+# 			else
+# 				_to_clone+=($(git remote -v show origin | grep Fetch | cut -d":" -f2- | tr -d '[:space:]'))
+# 			fi
+# 		fi
+# 	done
+# fi
+#
+# rm -rf "$GOPATH"
+# mkdir -p "${_current_employer_gopath}"
+#
+# if [[ ${_unpushed_changes} == 1 ]]; then
+# 	mkdir -p "${_current_employer_gopath}"
+# 	cd "${_current_employer_gopath}"
+# 	for _repo in ${_dirty_repos[*]}; do
+# 		cd "${_current_employer_gopath}"
+# 		mv /tmp/"${_repo}" .
+# 		cd "${_repo}"
+# 		git remote prune origin
+# 	done
+# fi
+#
+# if [[ ${#_to_clone[*]} -gt 0 ]]; then
+# 	for _url in ${_to_clone[*]}; do
+# 		cd "${_current_employer_gopath}"
+# 		git clone "${_url}"
+# 	done
+# fi
 
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install
