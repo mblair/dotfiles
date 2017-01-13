6eda2e2c4dfafbbd772f54204bfb59f79d63ee77
Matt Blair <me@matthewblair.net>
2017-01-12 20:26:35 -0800

    updates for new mac

--- mac_update_things.sh
+++ mac_update_things.sh
@@ -58,9 +58,8 @@ ${_HERE}/install.sh
 brew install autojump bash loc ffmpeg git git-extras gnu-sed gnupg irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi keybase nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid ripgrep unrar mtr ccat watch
 brew install --devel go
 brew install --HEAD hub
-brew install --with-toolchain --with-all-targets llvm
 brew install curl --with-nghttp2
-brew cask install hab emacs
+brew cask install hab emacs java
 brew install go-delve/delve/delve
 
 ${_HERE}/go_clean.sh
