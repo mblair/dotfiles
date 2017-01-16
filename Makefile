7dd992e2539447a11e0efbdfd0d6f9c3c92bfe53
Matt Blair <me@matthewblair.net>
2017-01-16 11:10:57 -0800

    cleanup

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+.PHONY: all
+all: fmt check
+
 .PHONY: fmt
 fmt:
 	shfmt -w *.sh
 	markdownfmt -w *.md
 
+.PHONY: check
 check:
 	shellcheck *.sh
--- mac_update_things.sh
+++ mac_update_things.sh
@@ -56,16 +56,16 @@ ${_HERE}/install.sh
 #/usr/local/bin/emacs --daemon
 
 for _pkg in autojump bash ffmpeg git git-extras gnu-sed gnupg2 irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi keybase nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid unrar mtr ccat watch go hub go-delve/delve/delve; do
-    _install_flags=""
-    if [[ ${_pkg} == "go" ]]; then
-        _install_flags="--devel"
-    elif [[ ${_pkg} == "hub" ]]; then
-        _install_flags="--HEAD"
-    elif [[ ${_pkg} == "curl" ]]; then
-        _install_flags="--with-nghttp2"
-    fi
+	_install_flags=""
+	if [[ ${_pkg} == "go" ]]; then
+		_install_flags="--devel"
+	elif [[ ${_pkg} == "hub" ]]; then
+		_install_flags="--HEAD"
+	elif [[ ${_pkg} == "curl" ]]; then
+		_install_flags="--with-nghttp2"
+	fi
 
-    brew install ${_install_flags} ${_pkg} || brew upgrade ${_pkg}
+	brew install ${_install_flags} ${_pkg} || brew upgrade ${_pkg}
 done
 
 brew cask install hab emacs java
