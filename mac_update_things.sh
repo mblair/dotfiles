5c1621a833e266b3535993900c90b58c249bd030
Matt Blair <me@matthewblair.net>
2017-05-27 21:42:20 -0700

    mac updater: actually install curl

--- mac_update_things.sh
+++ mac_update_things.sh
@@ -62,7 +62,7 @@ ${_HERE}/install.sh
 #/usr/local/bin/emacs --daemon
 
 brew tap caskroom/fonts
-for _pkg in autojump bash ffmpeg git git-extras gnu-sed gnupg irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid unrar mtr ccat watch go hub heroku emacs httpstat clang-format ctop certbot pngcheck pandoc git-lfs go-delve/delve/delve; do
+for _pkg in autojump bash ffmpeg git git-extras gnu-sed gnupg irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid unrar mtr ccat watch go hub heroku emacs httpstat clang-format ctop certbot pngcheck pandoc curl git-lfs go-delve/delve/delve; do
 	_install_flags=""
 	if [[ ${_pkg} == "hub" ]]; then
 		_install_flags="--HEAD"
