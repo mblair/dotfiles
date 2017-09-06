7b78b2b6f8fc02aaba81483bc3d9bf3fd311b8af
Matt Blair <me@matthewblair.net>
2017-09-06 11:13:57 -0700

    mac updater: more packages, stable go

--- mac_update_things.sh
+++ mac_update_things.sh
@@ -63,12 +63,10 @@ ${_HERE}/install.sh
 #/usr/local/bin/emacs --daemon
 
 brew tap caskroom/fonts
-for _pkg in autojump bash ffmpeg git git-extras gnu-sed gnupg irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid unrar mtr ccat watch go hub heroku emacs httpstat clang-format ctop certbot pngcheck pandoc curl git-lfs docker-machine-driver-xhyve go-delve/delve/delve; do
+for _pkg in autojump bash ffmpeg git git-extras gnu-sed gnupg irssi jq macvim python python3 s3cmd shellcheck ssh-copy-id the_silver_searcher tmux wget youtube-dl zsh findutils ghi nginx postgresql redis phantomjs pup vault wget httpdiff gifsicle yarn zsh-completions wifi-password cowsay node jid unrar mtr ccat watch go hub heroku emacs httpstat clang-format ctop certbot pngcheck pandoc curl git-lfs exa docker-machine-driver-xhyve azure-cli go-delve/delve/delve; do
 	_install_flags=""
 	if [[ ${_pkg} == "hub" ]]; then
 		_install_flags="--HEAD"
-	elif [[ ${_pkg} == "go" ]]; then
-		_install_flags="--devel"
 	elif [[ ${_pkg} == "curl" ]]; then
 		_install_flags="--with-nghttp2"
 	elif [[ ${_pkg} == "emacs" ]]; then
