3ac9df25280ac98aa8bfa3aad9232c82e007c062
Matt Blair <me@matthewblair.net>
2018-12-17 07:58:18 -0800

    add public-ip

--- mac_update_things.sh
+++ mac_update_things.sh
@@ -67,7 +67,7 @@ ln -sf ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal
 #fi
 
 npm install -g npm
-npm install -g grunt-cli redis-dump rickshaw jquery bootstrap react underscore d3 coffee-script webtorrent-cli js-yaml how2 eslint create-react-app parsimmon exif standard standard-format write-good fast-cli prettier js-beautify hyperapp wunderline ndb bash-language-server
+npm install -g grunt-cli redis-dump rickshaw jquery bootstrap react underscore d3 coffee-script webtorrent-cli js-yaml how2 eslint create-react-app parsimmon exif standard standard-format write-good fast-cli prettier js-beautify hyperapp wunderline ndb bash-language-server public-ip-cli
 
 ${_HERE}/install.sh
 
