708c2c5622d913e5e44313f11f5148ddb72ce49c
Matt Blair <me@matthewblair.net>
2019-01-10 15:53:16 -0800

    node either

--- install.sh
+++ install.sh
@@ -29,15 +29,12 @@ else
 	#if ! which docker; then
 	#	curl -sSL https://get.docker.com/ | sh
 	#fi
-	#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-	apt-get -y install nodejs
 	curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | bash
 	apt-get update
 	apt-get -y dist-upgrade
 	apt-get -y install autojump silversearcher-ag git emacs25-nox vim htop curl wget tmux jq ruby python build-essential strace locate tcpdump shellcheck mtr traceroute iftop auditd reptyr zsh whois moreutils
 	chsh -s /bin/zsh
 	apt-get -y purge unattended-upgrades lxd snapd lxcfs
-	npm install -g prettier
 
 	_AUDITD_RESTART=0
 	if ! sudo grep -q execve /etc/audit/audit.rules; then
