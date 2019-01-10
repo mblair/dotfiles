904c0646f2d91bc1af792e6130b0e2c3a6de4c24
Matt Blair <me@matthewblair.net>
2019-01-10 15:47:15 -0800

    update versions

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ _HERE=$(
 
 source ${_HERE}/vcs.bash
 
-_HUB_VER="2.6.0"
+_HUB_VER="2.7.0"
 _CTOP_VER="0.7.1"
-_GO_VER="1.11.2"
+_GO_VER="1.11.4"
 
 if [[ $(uname -s) == "Darwin" ]]; then
 	if ! brew list -1 | grep wget; then
