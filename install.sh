0ad3b7199832e317b6c2e3105a943002cd75e19b
Matt Blair <me@matthewblair.net>
2019-07-24 21:41:07 -0700

    try this

--- install.sh
+++ install.sh
@@ -152,6 +152,7 @@ if [[ $(uname -s) == "Linux" ]]; then
 
 	cd
 	export GOPATH=$HOME/go
+    export PATH="$HOME/go/bin:$PATH"
 	mkdir -p go/src/github.com/mblair/matthewblair.net
 	if [[ ! -d go/src/github.com/mblair/matthewblair.net/.git ]]; then
 		git clone https://github.com/mblair/matthewblair.net go/src/github.com/mblair/matthewblair.net
