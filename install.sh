daa51b029461335fb8d95532a8846fc70a1f9bcc
Matt Blair <me@matthewblair.net>
2023-07-08 07:01:58 -0700

    install gh cli

--- install.sh
+++ install.sh
@@ -148,6 +148,13 @@ if [[ $(uname -s) == "Linux" ]]; then
 		curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
 	fi
 
+	type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+		sudo apt update &&
+		sudo apt install gh -y
+
 	cp /usr/share/zoneinfo/UTC /etc/localtime || true
 
 	if pgrep snapd >/dev/null 2>&1; then
