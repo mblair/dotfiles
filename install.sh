302dac5ada97a7687710ffa4caeaf990c0c99e7a
Matt Blair <me@matthewblair.net>
2023-10-09 14:37:54 -0700

    karabiner fix, emacs font change

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ EOF
 	ln -sf ~/my_src/dotfiles/prelude/personal.el ~/.emacs.d/personal
 
 	mkdir -p ~/.config/karabiner/
-	ln -sf ~/my_src/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+	cp ~/my_src/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 else
 	if ! which docker; then
 		curl -sSL https://get.docker.com/ | sh
--- prelude/personal.el
+++ prelude/personal.el
@@ -19,7 +19,7 @@
 ;;(load-theme 'warm-night t)
 (load-theme 'ariake t)
 (set-face-attribute 'default nil :height 140)
-(set-face-attribute 'default nil :family "Andale Mono")
+(set-face-attribute 'default nil :family "B612 Mono")
 (setq whitespace-style (delete 'lines-tail whitespace-style))
 (defun my/setup-go-mode-gofmt-hook ()
   ;; Use goimports instead of go-fmt
