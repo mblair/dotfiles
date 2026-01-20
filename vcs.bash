442113340f4f9ff528ae0f5ff0684e56306ccde7
Matt Blair <me@matthewblair.net>
2026-01-20 13:18:47 +0000

    autostash

--- vcs.bash
+++ vcs.bash
@@ -1,52 +1,46 @@
 #!/usr/bin/env bash
 
 pretty_date() {
-    _TS=$1
-    _FORMAT='+%Y%m%dT%H%M'
+	_TS=$1
+	_FORMAT='+%Y%m%dT%H%M'
 
-    if [[ $(uname -s) == 'Darwin' ]]; then
-        date -u -j -f '%s' "${_TS}" "${_FORMAT}"
-    else
-        date -u --date @"${_TS}" "${_FORMAT}"
-    fi
+	if [[ $(uname -s) == 'Darwin' ]]; then
+		date -u -j -f '%s' "${_TS}" "${_FORMAT}"
+	else
+		date -u --date @"${_TS}" "${_FORMAT}"
+	fi
 }
 
 git_revid() {
-    git log -n1 --format='%H'
+	git log -n1 --format='%H'
 }
 
 git_epoch() {
-    git log -n1 --format='%at'
+	git log -n1 --format='%at'
 }
 
 git_branch() {
-    git symbolic-ref --short -q HEAD
+	git symbolic-ref --short -q HEAD
 }
 
 git_clean() {
-    git clean -fdx
-    git reset --hard
+	git clean -fdx
+	git reset --hard
 }
 
 git_update() {
-    _default_branch=$(git remote show origin | grep 'HEAD branch' | perl -pe 's|HEAD branch: (.*)|${1}|g' | awk '{print $1}')
-    _current_branch=$(git branch --show-current)
-    git checkout "${_default_branch}"
-    git branch --set-upstream-to=origin/"${_default_branch}" "${_default_branch}"
+	_default_branch=$(git remote show origin | grep 'HEAD branch' | perl -pe 's|HEAD branch: (.*)|${1}|g' | awk '{print $1}')
+	_current_branch=$(git branch --show-current)
+	git checkout "${_default_branch}"
+	git branch --set-upstream-to=origin/"${_default_branch}" "${_default_branch}"
 
-    if [[ -f .gitmodules ]]; then
-        git submodule update --init
-    fi
-    if git diff-index --quiet HEAD; then
-        git pull
-    else
-        git stash
-        git pull
-        git stash pop
-    fi
-    if [[ $_default_branch != "$_current_branch" ]]; then
-        git checkout "${_current_branch}"
-    fi
+	if [[ -f .gitmodules ]]; then
+		git submodule update --init
+	fi
+	git pull --rebase --autostash
+	if [[ $_default_branch != "$_current_branch" ]]; then
+		git checkout "${_current_branch}"
+	fi
 
-    git-delete-merged-branches
+	git-delete-merged-branches
 }
